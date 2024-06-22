d21b3aeba8ed1632b1c346327864393473cf69a3
dannnnnm <137915605+dannnnnm@users.noreply.github.com>
2024-06-22 17:05:37 -0400

    script no se utilizara realmente, es parte de iniciar una partida

---
+++ src/game/js/script.ts
@@ -0,0 +1,99 @@
+//Ejercicio de practica Javascript
+
+import { Game } from "./lecs/game.js";
+
+
+
+  
+    
+ 
+
+  
+
+/*var audio=new Audio("audio/freedoom.m4a")
+audio.volume=0.3
+audio.loop=true
+
+let toggleMusic=document.getElementById("stopMusic")
+toggleMusic.onclick=function(){
+  console.log("called")
+  if (!audio.paused){
+    audio.pause()
+  }
+  else{
+    audio.play()
+    document.getElementById("statusParagraph").textContent=""
+    
+  }
+  toggleMusic.blur()
+}
+
+var changeMusic=function(name){
+  audio.pause()
+  audio=new Audio(name)
+  audio.volume=0.25
+  audio.loop=true
+  audio.play()
+  document.getElementById("statusParagraph").textContent=""
+}
+
+let selectDefaultMusic=document.getElementById("defaultMusic")
+selectDefaultMusic.onclick=function(){
+  changeMusic("audio/freedoom.m4a")
+  selectDefaultMusic.blur()
+}
+let selectEdmMusic=document.getElementById("edmMusic")
+selectEdmMusic.onclick=function(){
+  changeMusic("audio/princess.m4a")
+  selectEdmMusic.blur()
+}
+let selectRelMusic=document.getElementById("reloadedMusic")
+selectRelMusic.onclick=function(){
+  changeMusic("audio/Batallapkmn.mp3")
+  selectRelMusic.blur()
+}
+
+audio.play().then(
+  (result)=>{
+    audio.muted=false
+  },
+  (error)=>{
+    let paragraph=document.getElementById("statusParagraph")
+    paragraph.textContent="(Could not autoplay music)"
+    
+    
+    
+  }
+)*/
+
+
+/*
+let game;
+
+window.startGame=function(p1char,p2char){
+  game=new Game(p1char,p2char)
+  game.start()
+}
+window.stopGame=function(){
+  clearTimeout(game.gameUpdateId)
+  game.stop()
+}
+window.getPlayer1=function(){
+  return game.getPlayer1()
+}
+
+window.getPlayer2=function(){
+  return game.getPlayer2()
+}
+
+window.clearScript=()=>{
+  game=null;
+}
+//game.start();
+let resetButton=document.getElementById("resetButton")
+
+
+export function clickResetButton(){
+  //resetButton.click()
+}*/
+
